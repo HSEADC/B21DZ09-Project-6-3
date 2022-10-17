9cdbd948db2a5ec6f34c0599ab08370f069a8635
Samiul Haque Sohan <samiulhsohan@gmail.com>
2022-10-17 20:59:51 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './index.css'
+
+import './javascript/index'
+import './javascript/about'
+import './javascript/collections'
+import './javascript/lifehacks'
+import './javascript/map'
+import './javascript/remindercard'
