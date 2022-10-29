e70e1d0d7d99f1e3acae339cfd7230789da4e803
Samiul Haque Sohan <samiulhsohan@gmail.com>
2022-10-29 13:11:46 +0300

    integration

--- src/javascript/hamburger.js
+++ src/javascript/hamburger.js
@@ -2,9 +2,11 @@ import telegramLogo from '../images/telegram.svg'
 import vkLogo from '../images/vk.svg'
 import instagramLogo from '../images/instagram.svg'
 import recycleLogo from '../images/promo/recycle.svg'
-
+import searchIcon from '../images/search.svg'
+import backIcon from '../images/arrow.svg'
 
 const hamburger = document.querySelector('#hamburger')
+const pageHamburger = document.querySelector('#page-hamburger')
 
 function toggleHamburger() {
   document.querySelector('.menu__container').classList.toggle('hidden')
@@ -12,12 +14,26 @@ function toggleHamburger() {
   document.querySelectorAll('.hamburger__line').forEach((line) => {
     line.classList.toggle('hidden')
   })
-
 }
 
-hamburger.addEventListener('click', toggleHamburger)
+function togglePageHamburger() {
+  document.querySelector('.page-navbar__container').classList.toggle('hidden')
+  document.querySelector('#hamburger-close').classList.toggle('hidden')
+  document.querySelectorAll('.hamburger__line').forEach((line) => {
+    line.classList.toggle('hidden')
+  })
+}
+
+hamburger?.addEventListener('click', toggleHamburger)
+pageHamburger?.addEventListener('click', togglePageHamburger)
 
 document.querySelector('.telegram-logo').src = telegramLogo
 document.querySelector('.insta-logo').src = instagramLogo
 document.querySelector('.vk-logo').src = vkLogo
 document.querySelector('.recycle-logo').src = recycleLogo
+
+// search icon
+document.querySelector('.search-icon').src = searchIcon
+
+// back icon
+document.querySelector('.back-icon').src = backIcon
