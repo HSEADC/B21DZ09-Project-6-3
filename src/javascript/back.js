b6cf963da5168731163e52b25cdf2fa27cc73933
stanislavasal <90899256+stanislavasal@users.noreply.github.com>
2023-05-31 08:35:16 +0300

    procces

--- src/javascript/back.js
+++ src/javascript/back.js
@@ -1,7 +1,12 @@
-const backButton = document.querySelector('#back-button')
-
-if (backButton) {
-  backButton.addEventListener('click', () => {
-    window.history.back()
+document.addEventListener('DOMContentLoaded', function () {
+  const backButton = document.querySelector('.Q_GoBack')
+  backButton.addEventListener('click', function () {
+    if (document.referrer) {
+      window.location.href = document.referrer
+    } else {
+      // Ваш fallback-код, если document.referrer недоступен
+      // Например, перенаправление на главную страницу
+      window.location.href = '/'
+    }
   })
-}
+})
