b2e2f57c31afdc8b643d11d6de42c12ef3290505
stanislavasal <90899256+stanislavasal@users.noreply.github.com>
2023-06-20 07:04:40 +0300

    Adaptives for Main Page process

--- src/javascript/burgermenu.js
+++ src/javascript/burgermenu.js
@@ -1,10 +1,16 @@
-const toggleButton = document.querySelector('.Q_Menu')
+const toggleButton = document.querySelector('.Q_BurgerMenuIqon')
 const overlay = document.querySelector('.C_Menu')
+const header = document.querySelector('.W_Header')
 
 toggleButton?.addEventListener('click', function () {
   if (overlay.style.display === 'none' || overlay.style.display === '') {
     overlay.style.display = 'block'
+    header.style.position = 'fixed'
+    header.style.top = '0'
+    document.body.style.paddingTop = header.offsetHeight + 'px'
   } else {
     overlay.style.display = 'none'
+    header.style.position = 'static'
+    document.body.style.paddingTop = '0'
   }
 })
