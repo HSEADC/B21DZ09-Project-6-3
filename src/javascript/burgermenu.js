325ef0a818ac1bf418e641cf59981af233f9a24c
stanislavasal <90899256+stanislavasal@users.noreply.github.com>
2023-06-22 10:08:37 +0300

    adding meta

--- src/javascript/burgermenu.js
+++ src/javascript/burgermenu.js
@@ -14,3 +14,28 @@ toggleButton?.addEventListener('click', function () {
     document.body.style.paddingTop = '0'
   }
 })
+
+const menuItems = document.querySelectorAll('.menu-item1')
+
+// Перебираем элементы и добавляем обработчик события наведения мыши
+menuItems.forEach((menuItem) => {
+  const symbol = menuItem.querySelector('.Q_MainPageSymbol')
+
+  menuItem?.addEventListener('mouseenter', () => {
+    // Изменяем цвет текста и картинку всех пунктов меню, кроме текущего
+    menuItems.forEach((item) => {
+      if (item !== menuItem) {
+        item.style.transition = 'color 0.3s, opacity 0.3s'
+        item.style.color = 'rgba(0, 0, 0, 0.4)'
+      }
+    })
+  })
+
+  // Восстанавливаем цвет текста и картинку всех пунктов меню при уходе мыши
+  menuItem?.addEventListener('mouseleave', () => {
+    menuItems.forEach((item) => {
+      item.style.transition = 'color 0.3s, opacity 0.3s'
+      item.style.color = 'rgba(0, 0, 0, 1)'
+    })
+  })
+})
